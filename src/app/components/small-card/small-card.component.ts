8d5c1d19dcc86aa053030d848e4a5fedbf97150a
Kaio Dantas Mancini <dantaskaio01@gmail.com>
2023-09-02 14:14:05 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -11,6 +11,8 @@ export class SmallCardComponent implements OnInit {
   @Input()
   cardDescription: string = "";
   constructor() { }
+  @Input()
+  Id:string = "0";
 
   ngOnInit(): void {
   }
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Elden Ring é eleito o jogo do ano",
+        "description":"O ano de 2022 fechou com Elden Ring como o maior sucesso. O mais recente título de Hidetaka Miyazaki e FromSoftware varreu as premiações de títulos do ano passado. Para se consolidar ainda mais, o maior soulslike começa 2023 com mais prêmios, incluindo o de jogo do ano, pelo New York Game Awards, que está em sua 12ª edição.",
+        "photoCover": "https://i.redd.it/2b6agzgihmj81.jpg"
+    },
+
+    {
+        "id":"2",
+        "title":"Anunciao Remake do classíco Demons Souls para PS5",
+        "description":"O amado jogo da trilogia Souls anuncia sua chegada para os consoles da nova geração, abaixo tudo o que você precisa saber antes de iniciar suas aventuras por essa obra",
+        "photoCover": "https://image.api.playstation.com/vulcan/img/rnd/202011/1717/M8cxD6UPmsU551lmNq9Lat9Y.png"
+    },
+    {
+        "id":"3",
+        "title":"Acompanhe tudo sobre o lançamento de LIES OF PI",
+        "description":"O amado jogo da trilogia Souls anuncia sua chegada para os consoles da nova geração, abaixo tudo o que você precisa saber antes de iniciar suas aventuras por essa obra",
+        "photoCover": "https://image.api.playstation.com/vulcan/ap/rnd/202305/2308/d468cc0c5e578ea5ba4e246aa9be258c4137a025b408214c.png"
+    },
+    {
+        "id":"4",
+        "title":"Entenda sobre a lore de MIDIR",
+        "description":"Darkeater Midir can be first glimpsed upon entering the city, although he will not interact with the player until they reach the bridge past the abyssal swamp. He will initially wait on the cliff for the player to approach at which point he will swoop down and breathe fire along the bridge and connecting path.Immediately after exiting the Shared Grave bonfire, Midir will land on the bridge and block the path to the Church of Filianore. After sustaining enough damage he will fall into the chasm under the bridge. A secret passage accessed via the Ringed Inner Wall's second shortcut leads to the chasm where Midir can be fought as the DLC's optional boss.A descendant of the Archdragons and raised by the gods themselves, Midir was tasked with fighting against the abyss and its creations. His immortality enables him to perform his duties without end, even after the gods have long since perished. But his task has rendered him damaged beyond repair, as the dark has seeped into him and infected him (his title implies that he actually consumed potent sources of dark), and forms black crystals. It is not exactly clear whether or not he has been fully consumed, but he clearly feels the effects, able to use the dark in combat.Within the memories of Midir is an old remembrance of a sword, similar to that of Seath's moonlight power, but the memory was from not long after the Beginning.",
+        "photoCover": "https://cdna.artstation.com/p/assets/images/images/026/853/568/large/igor-soares-dark-souls-3-medir.jpg?1589908962"
+    }
+]
